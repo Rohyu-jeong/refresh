418bb3ea4c661281e443deb84cb1bd260cadce06
Rohyj <yacheku@gmail.com>
2024-10-28 13:03:42 +0900

    20241028 cors에 vercel 배포 주소 추가

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
 
   // CORS 설정
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'https://refresh-beryl.vercel.app/'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
