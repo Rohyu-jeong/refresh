73b2ac818021725fcad0a38c5e13ee974d372518
Rohyj <yacheku@gmail.com>
2024-10-28 14:56:01 +0900

    배포용으로 변경 로컬 안됨

---
+++ api/main.ts
@@ -0,0 +1,39 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../src/app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+
+
+const server = express();
+
+export const createNestServer = async (expressInstance) => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+
+  // 전역 파이프 및 기타 설정
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // DTO에 정의되지 않은 속성 제거
+      forbidNonWhitelisted: true, // DTO에 정의되지 않은 속성이 포함된 경우 에러 발생
+      transform: true, // 자동 변환
+    }),
+  );
+
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://your-frontend-domain.com',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  await app.init();
+};
+
+createNestServer(server);
+
+export default server;
