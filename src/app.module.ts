f5345bf5e7f0dfa0175d402daa1f2b9c3945ebd9
Rohyj <yacheku@gmail.com>
2024-10-28 14:21:36 +0900

    20241028 postgres로 db 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,14 +15,17 @@ import { RefreshToken } from './refresh_token/entities/refresh_token.entity';
       isGlobal: true, // 환경 변수를 전역으로 사용
     }),
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DATABASE_HOST || 'localhost',
-      port: parseInt(process.env.DATABASE_PORT) || 3306,
-      username: process.env.DATABASE_USERNAME || 'admin',
-      password: process.env.DATABASE_PASSWORD || '1234',
-      database: process.env.DATABASE_NAME || 'nest_auth_db',
+      type: 'postgres',
+      host: process.env.DATABASE_HOST || 'ep-nameless-firefly-a49bi3zp-pooler.us-east-1.aws.neon.tech',
+      port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+      username: process.env.DATABASE_USERNAME || 'default',
+      password: process.env.DATABASE_PASSWORD || 'M2EcWsg4oplI',
+      database: process.env.DATABASE_NAME || 'verceldb',
       entities: [User, RefreshToken],
       synchronize: true,
+      ssl: {
+        rejectUnauthorized: false, // ssl 검증하지 않음
+      }
     }),
     AuthModule,
     UsersModule,
